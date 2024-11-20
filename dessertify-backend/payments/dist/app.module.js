62efb24942a38e27412e1d4001e2e854adad8842
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-11-20 15:37:27 -0300

    Remove submódulo dessertify-backend/payments

---
+++ dessertify-backend/payments/dist/app.module.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const Joi = require("joi");
+const payments_controller_1 = require("./payments.controller");
+const payments_service_1 = require("./payments.service");
+const prisma_service_1 = require("./prisma.service");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                validationSchema: Joi.object({
+                    RABBITMQ_URL: Joi.string().required(),
+                    DATABASE_URL: Joi.string().required(),
+                }),
+            }),
+        ],
+        controllers: [payments_controller_1.PaymentsController],
+        providers: [payments_service_1.PaymentsService, prisma_service_1.PrismaService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
---
+++ dessertify-backend/payments/dist/payments.controller.d.ts
@@ -0,0 +1,3 @@
+export declare class PaymentsController {
+    getHello(data: any): void;
+}
