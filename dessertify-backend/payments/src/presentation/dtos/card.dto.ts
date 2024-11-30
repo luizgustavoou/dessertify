f211eef1c29a28ee29a17dcae3014698b6669ffe
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-11-30 20:17:27 -0300

    ajeitar create carge dto

--- dessertify-backend/payments/src/presentation/dtos/card.dto.ts
+++ dessertify-backend/payments/src/presentation/dtos/card.dto.ts
@@ -1,16 +1,24 @@
-import { IsCreditCard, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Expose } from 'class-transformer';
+import { IsCreditCard, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CardDto {
+  @Expose()
   @IsString()
   @IsNotEmpty()
   cvc: string;
 
+
+  @Expose()
+  @IsOptional()
   @IsNumber()
   exp_month?: number;
 
+  @Expose()
+  @IsOptional()
   @IsNumber()
   exp_year?: number;
 
+  @Expose()
   @IsCreditCard()
   number: string;
 }
