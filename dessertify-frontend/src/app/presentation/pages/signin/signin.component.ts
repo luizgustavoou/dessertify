6ea7164183a8e930c7c915843ba09f3ac49500e2
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-03-02 12:44:58 -0300

    feat: :sparkles: corrigir cores

--- dessertify-frontend/src/app/presentation/pages/signin/signin.component.ts
+++ dessertify-frontend/src/app/presentation/pages/signin/signin.component.ts
@@ -19,10 +19,11 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   standalone: true,
   selector: 'app-signin',
+  styleUrl: './signin.component.scss',
   template: `
-    <div class="flex justify-center items-center min-h-screen p-6 bg-rose-100">
+    <div class="flex justify-center items-center min-h-screen p-6">
       <div
-        class="rounded-lg p-8 bg-rose-200/70 shadow-2xl flex flex-col justify-center items-center max-w-lg w-full"
+        class="login-container rounded-lg p-8 shadow-2xl flex flex-col justify-center items-center max-w-lg w-full"
       >
         <img
           src="./assets/images/logo-1.png"
